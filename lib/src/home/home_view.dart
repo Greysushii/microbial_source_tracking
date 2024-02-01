e884ddcf9b746ef389856f93c66d73615f4d0b03
Manpreet Singh <mxsingh13@gmail.com>
2024-01-31 22:29:32 -0500

    Home page sections are now based on lists

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomeListView extends StatelessWidget {
-  const HomeListView({super.key});
+  HomeListView({super.key});
+
+  final List<String> devices = <String>['Device 1', 'Device 2', 'Device 3'];
+  final List<String> recents = <String>[
+    '012924_upload.jpg',
+    '120223_upload.jpg',
+    '122723_upload.jpg',
+    '012724_upload.jpg',
+    '012824_upload.jpg',
+    '121523_upload.jpg',
+    '112723_upload.jpg',
+    '112923_upload.jpg',
+    '010224_upload.jpg',
+    '010324_upload.jpg',
+    '121323_upload.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,26 +26,41 @@ class HomeListView extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             const Padding(
-                padding: EdgeInsets.all(8.0), child: Text('Bluetooth Devices')),
+                padding: EdgeInsets.all(8.0),
+                child: Text('Bluetooth Devices',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
             Expanded(
-                child: ListView(
-              children: const [
-                ListTile(title: Text("Bluetooth Device 1")),
-                ListTile(title: Text("Bluetooth Device 2")),
-                ListTile(title: Text("Bluetooth Device 3"))
-              ],
-            )),
+              child: ListView.separated(
+                padding: EdgeInsets.all(8.0),
+                itemCount: devices.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 30,
+                    child: Text('Bluetooth ${devices[index]}'),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              ),
+            ),
             const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: Text('Most Recent Pictures')),
+                child: Text('Most Recent Pictures',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
             Expanded(
-                child: ListView(
-              children: const [
-                ListTile(title: Text("012924_upload.jpg")),
-                ListTile(title: Text("120223_upload.jpg")),
-                ListTile(title: Text("122723_upload.jpg"))
-              ],
-            )),
+              child: ListView.separated(
+                padding: EdgeInsets.all(8.0),
+                itemCount: recents.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 30,
+                    child: Text(recents[index]),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              ),
+            ),
           ],
         ));
   }
