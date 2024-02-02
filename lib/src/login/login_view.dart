469b1245e477a061e8514f59a42e91ccf8c75f38
Manpreet Singh <mxsingh13@gmail.com>
2024-02-02 13:11:47 -0500

    Created login page layout

---
+++ lib/src/login/login_view.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class LoginView extends StatelessWidget {
+  LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Container(
+                      alignment: Alignment.center,
+                      child: SvgPicture.asset('assets/images/glwa_logo.svg')
+                      /* Text(
+                      'Logo',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold),
+                    ),*/
+                      ),
+                  const SizedBox(height: 30),
+                  Text(
+                    'Login to your account',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    'Login widget goes here',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Login button goes here',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              )),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 50,
+        color: Colors.white,
+        alignment: Alignment.topCenter,
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text('Don\'t have an account? '),
+          InkWell(
+            child: Text(
+              'Sign up here',
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
