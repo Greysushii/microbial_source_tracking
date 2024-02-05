9000762065b01a85cf3990ed724f59b3bd4e9172
maazooshake <maazooshake@gmail.com>
2024-02-05 18:59:49 -0500

    cleaned up historypage code

--- lib/src/history/history_page.dart
+++ lib/src/history/history_page.dart
@@ -3,21 +3,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HistoryPage(),
-    );
-  }
-}
-
 class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
 
