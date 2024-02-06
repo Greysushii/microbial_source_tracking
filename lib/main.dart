8ea686354fb101cd961fdf78a1d5a249d619c623
NickZuke <123267015+NickZuke@users.noreply.github.com>
2024-02-05 22:50:43 -0500

    Registration page (#8)
    * Created account creation page and added barebones fields (email, password + password confirm) as placeholders
    
    * Added basic registration page as a base, only has
    fields for email, password, and password confirm.
    Working on proper firebase connection.
    
    * Added register button on account_creation_view to finalize registration (does nothing yet)
    
    * Firebase initialization, committing here incase I need to rollback to the first step of this process.
    
    * Testing authentication emulation as well as passing the email and password through to the authentication function.
    
    * Email and password are now stored on Firebase! Issue: Credential says it's not being used? (line 41) So, will need to see why that is.
    
    * Passwords can now be checked with one another, added alerts to confirm if registration was successful or not.
    
    * Commented out main
    
    * Commented out main for my page to remove conflicts
    with main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 //import 'package:microbial_source_tracking/src/login/login_view.dart';
 import 'package:microbial_source_tracking/src/home/home_view.dart';
