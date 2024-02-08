1ed3be4acab812363ae7226303a26b3982612b3b
Manpreet Singh <mxsingh13@gmail.com>
2024-02-07 23:21:23 -0500

    Fixing merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override // This widget is the root of our application
+  @override //This widget is the root of our application
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test',
