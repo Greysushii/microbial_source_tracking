bf40db3253814451bae4cea2f96bb9de1a233c55
Manpreet Singh <mxsingh13@gmail.com>
2024-03-07 07:43:05 -0500

    How to page for Bluetooth

--- lib/src/home/how_to/how_to_bluetooth.dart
+++ lib/src/home/how_to/how_to_bluetooth.dart
@@ -10,10 +10,10 @@ class HowToBluetooth extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: glwaTheme.secondaryHeaderColor,
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Column(
           children: [
-            Padding(
+            const Padding(
               padding: EdgeInsets.all(8.0),
               child: Center(
                 child: Text(
@@ -21,7 +21,42 @@ class HowToBluetooth extends StatelessWidget {
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
                 ),
               ),
-            )
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: const Text(
+                'To begin connecting to a Bluetooth device, make sure both the device that this application is running on and the device that you will be connecting to have their Bluetooth settings on.',
+                style: TextStyle(fontSize: 17),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: RichText(
+                  text: const TextSpan(children: [
+                TextSpan(
+                    text:
+                        'Once Bluetooth is functional on both devices, go to the configurations (',
+                    style: TextStyle(color: Colors.black, fontSize: 17)),
+                WidgetSpan(child: Icon(Icons.build_circle_outlined)),
+                TextSpan(
+                    text: ') page from the navigation bar.',
+                    style: TextStyle(color: Colors.black, fontSize: 17))
+              ])),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: const Text(
+                'Bluetooth devices will be available as a list on the configurations page. Find your device within the presented list and tap on it to connect.',
+                style: TextStyle(fontSize: 17),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: const Text(
+                'Your device should now be connected to this application!',
+                style: TextStyle(fontSize: 17),
+              ),
+            ),
           ],
         ),
       ),
