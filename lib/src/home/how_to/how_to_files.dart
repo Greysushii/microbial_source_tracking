90a512722e3d50b9ee41b375e3068bd81becbcaa
Manpreet Singh <mxsingh13@gmail.com>
2024-03-07 09:44:17 -0500

    How to page for uploading files

--- lib/src/home/how_to/how_to_files.dart
+++ lib/src/home/how_to/how_to_files.dart
@@ -10,10 +10,10 @@ class HowToFiles extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: glwaTheme.secondaryHeaderColor,
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Column(
           children: [
-            Padding(
+            const Padding(
               padding: EdgeInsets.all(8.0),
               child: Center(
                 child: Text(
@@ -21,7 +21,50 @@ class HowToFiles extends StatelessWidget {
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
                 ),
               ),
-            )
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: const Text(
+                'Once the image has been pulled from the Raspberry Pi, it will be stored locally on your device.',
+                style: TextStyle(fontSize: 17),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: RichText(
+                  text: const TextSpan(children: [
+                TextSpan(
+                    text:
+                        'To upload the image to the database, click on the folder icon (',
+                    style: TextStyle(fontSize: 17, color: Colors.black)),
+                WidgetSpan(child: Icon(Icons.folder_outlined)),
+                TextSpan(
+                    text:
+                        ') on the navigation bar. This opens up the history page, where you can view all the uploaded files.',
+                    style: TextStyle(fontSize: 17, color: Colors.black))
+              ])),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: const Text(
+                'There are two main buttons on the history page: Select File and Upload File. First, you will need to select a file from your device using the Select File. Your device\'s local files will be accessed and you will be able to select a file.',
+                style: TextStyle(fontSize: 17),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: const Text(
+                'Once selected, you will be prompted to rename the image. The selected file will appear on the history page. To upload this file, press the Upload File button.',
+                style: TextStyle(fontSize: 17),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              child: const Text(
+                'The image will be uploaded to the database and it will appear in a list of images on the history page.',
+                style: TextStyle(fontSize: 17),
+              ),
+            ),
           ],
         ),
       ),
