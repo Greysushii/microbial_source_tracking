355e3180c504926c0983f38034fa6ebcbb882ebc
Greysushii <kslatina@outlook.com>
2024-03-28 02:08:49 -0400

    Test work from Maaz for IOS and running dependencies for location services

--- lib/src/history/history_page.dart
+++ lib/src/history/history_page.dart
@@ -6,8 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:geolocator/geolocator.dart';
+// import 'package:geolocator_apple/geolocator_apple.dart';
+// import 'package:geolocator_android/geolocator_android.dart';
 import 'package:permission_handler/permission_handler.dart';
-
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
@@ -40,11 +41,41 @@ class _HistoryPageState extends State<HistoryPage> {
 
   Future uploadFile() async {
     
-    var locationPermissionStatus = await Permission.location.request();
+    bool serviceEnabled;
+    LocationPermission permission;
 
-    if (locationPermissionStatus == PermissionStatus.granted) {
-      
+  // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the 
+      // App to enable the location services.
+      print('location disabled');
+      return Future.error('Location services are disabled.');
+    }
 
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale 
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately. 
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+    } 
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+  
       final path = 'images/${pickedFile!.name}';
       final file = File(pickedFile!.path!);
 
@@ -93,7 +124,8 @@ class _HistoryPageState extends State<HistoryPage> {
           ),
         );
       }
-    }
+    
+    print(Geolocator.getCurrentPosition());
   }
 
   Future getWaterSource() async {
